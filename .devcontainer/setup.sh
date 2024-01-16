8477c9eb46413ff46457d40023010d1a863776ca
Antal Kenderesi <43848302+alkenderesi@users.noreply.github.com>
2024-01-16 23:36:04 +0100

    Add devcontainer config

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Install pip packages:
+# https://pip.pypa.io/en/stable/cli/pip/
+# https://pip.pypa.io/en/stable/cli/pip_install/
+pip3 install --cache-dir=.devcontainer/.cache/pip -r requirements.txt
